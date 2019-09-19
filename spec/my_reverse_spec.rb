25b494d2011cceab71835f227bf15c4504a75bfe
Toby <tobybrock93@gmail.com>
2019-09-19 16:01:10 +0100

    refactor of previous commit

--- spec/my_reverse_spec.rb
+++ spec/my_reverse_spec.rb
@@ -5,7 +5,7 @@ describe 'my_reverse' do
     expect(my_reverse([1, 2])).to eq([2, 1])
   end
   it 'reverses a different simply array' do
-    expect(my_reverse(['a', 'b'])).to eq(['b', 'a'])
+    expect(my_reverse(%w[a b])).to eq(%w[b a])
   end
   it 'matches the inbuilt .reverse method for complex arrays' do
     test_array = * (1..5000).map { rand }
