f0a7084ebabfa05c0a60cf7d14cface0b9dd0429
Toby <tobybrock93@gmail.com>
2019-09-19 16:00:42 +0100

    finished TDDing my_reverse algorithm

--- lib/my_reverse.rb
+++ lib/my_reverse.rb
@@ -1,3 +1,9 @@
 def my_reverse(array)
-    [2, 1]
+  i = 1
+  reversed_array = []
+  array.each do |x|
+    reversed_array[array.length - i] = x
+    i += 1
+  end
+  reversed_array
 end
--- spec/my_reverse_spec.rb
+++ spec/my_reverse_spec.rb
@@ -4,4 +4,11 @@ describe 'my_reverse' do
   it 'reverses a simple array' do
     expect(my_reverse([1, 2])).to eq([2, 1])
   end
+  it 'reverses a different simply array' do
+    expect(my_reverse(['a', 'b'])).to eq(['b', 'a'])
+  end
+  it 'matches the inbuilt .reverse method for complex arrays' do
+    test_array = * (1..5000).map { rand }
+    expect(my_reverse(test_array)).to eq(test_array.reverse)
+  end
 end
