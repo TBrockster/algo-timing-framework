93da13b7747f4d79365d4a0968681e8c46850db9
Toby <tobybrock93@gmail.com>
2019-09-19 13:03:13 +0100

    reworked algorithm timer

--- timer.rb
+++ timer.rb
@@ -1,11 +1,18 @@
 require 'benchmark'
 
-def timer(algorithm, array)
-    array.shuffle
-    algorithm_time = Benchmark.realtime do
-        array.sort
+def timer(steps)
+  results = {}
+  (1..steps).each do |step|
+    n = step * 5_000
+    array = * (1..n).map { rand }
+    algo_time = Benchmark.realtime do
+      array.dup.last
     end
-    puts algorithm_time
+    results[n] = [algo_time]
+  end
+  results.each do |result|
+    puts result
+  end
 end
 
-timer(1, [1,2,3])
+timer(20)
