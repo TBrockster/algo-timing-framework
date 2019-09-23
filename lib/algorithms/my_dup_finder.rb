3d63a27c65e729b3fa40fba9d4a963ba5ff7f8b5
Toby <tobybrock93@gmail.com>
2019-09-23 13:48:36 +0100

    written and passed 2nd my_dup_finder test

--- lib/algorithms/my_dup_finder.rb
+++ lib/algorithms/my_dup_finder.rb
@@ -1,3 +1,12 @@
 def my_dup_finder(array)
-    ['hello']
+  count = Hash.new(0)
+  duplicates = []
+  array.each do |word|
+    count[word] += 1
+    if count[word] == 2
+      p count[word]
+      duplicates << word
+    end
+  end
+  duplicates
 end
--- spec/my_dup_finder_spec.rb
+++ spec/my_dup_finder_spec.rb
@@ -4,4 +4,7 @@ describe 'my_dup_finder' do
   it 'when given an array, returns an array containing all duplicates' do
     expect(my_dup_finder(%w[hello world hello friend])).to eq(%w[hello])
   end
+  it 'when given a different array, returns an array containing all duplicates' do
+    expect(my_dup_finder(%w[the the house])).to eq(%w[the])
+  end
 end
